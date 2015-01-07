4815801d0db49064cf2ffcdca2e1a504c3e2fc76
Babu Lal Swami <swamibabulal@gmail.com>
2015-01-06 18:52:39 -0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'signup'    => 'user#new'
+  get 'signup'    => 'users#new'
   get 'login'   => 'static_pages#login'
   
+  resources :users
   
   
   
